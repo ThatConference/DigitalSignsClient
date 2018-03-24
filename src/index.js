6b715ae9555fdfe8e977f72e0ee1f9c8909b57ce
Clark Sell <clark.sell@csell.net>
2018-03-24 12:56:30 -0500

    added tests

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 
-import React from "react";
-import { render } from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
-import Router from "./components/Router";
+import Router from './components/Router';
 // import App from "./components/App";
 
-import registerServiceWorker from "./registerServiceWorker";
+import registerServiceWorker from './registerServiceWorker';
 
-import "./normalize.css";
-import "./index.css";
+import './normalize.css';
+import './index.css';
 
 const client = new ApolloClient({
-  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+    uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
 });
 
 registerServiceWorker();
 
 const root = () => (
-  <ApolloProvider client={client}>
-    <Router />
-  </ApolloProvider>
+    <ApolloProvider client={client}>
+        <Router />
+    </ApolloProvider>
 );
 
-render(root(), document.getElementById("root"));
+render(root(), document.getElementById('root'));
