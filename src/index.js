0d6d57c7a9faca19364b39597d8bad6ec6adcc40
Clark Sell <clark.sell@csell.net>
2018-03-24 07:54:39 -0500

    added apollo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
 import React from "react";
 import { render } from "react-dom";
 
 import Router from "./components/Router";
 import App from "./components/App";
 
-import "./normalize.css";
-import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 
-render(<Router />, document.getElementById("root"));
+import "./normalize.css";
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
 
 registerServiceWorker();
+
+const root = () => (
+  <ApolloProvider client={client}>
+    <Router />
+  </ApolloProvider>
+);
+
+render(root(), document.getElementById("root"));
