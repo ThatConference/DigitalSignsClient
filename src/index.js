f386a84ed7dd6dd61ab47765fb384c581263f083
Clark Sell <clark.sell@csell.net>
2018-04-15 16:45:53 -0500

    urls updated

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const httpLink = new HttpLink({
 });
 
 const wsLink = new WebSocketLink({
-  ws: 'ws://bigredbutton-165918.appspot.com/subscription',
+  ws: 'wss://bigredbutton-165918.appspot.com/subscription',
   // uri: 'ws://localhost:8000/subscriptions',
   options: {
     reconnect: true,
